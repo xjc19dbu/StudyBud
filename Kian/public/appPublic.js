b447effa1d245659d7b3484e5641404d83dafc26
xjc19dbu <98750256+xjc19dbu@users.noreply.github.com>
2022-05-15 23:27:05 +0100

    Add files via upload

--- Kian/public/appPublic.js
+++ Kian/public/appPublic.js
@@ -46,7 +46,7 @@ function onSignInClick(event){
         .then(d => {
             console.log(d)
             if(d == true){
-                location.href = 'home.html';
+                location.href = 'gantt2.html';
             }
             else{
                 alert("Sign in unsuccessful")
@@ -56,104 +56,6 @@ function onSignInClick(event){
         });
     return;
 }
-
-function infoBoxPop(id){
-    for(let i = 0;i < data.length;i++){
-        let Module = data[i].Module;
-        for(const [key, value] of Object.entries(data[i])){
-            if(key == "Tasks"){
-                let tasks = value.split("||")
-                for(let z = 0; z < tasks.length;z++){
-                    let dataid = Module + " " + tasks[z];
-                    if(dataid == id){
-                        let Activities = data[i].Activities.split("||");
-                        document.getElementById("activities").innerText = Activities[z];
-                        let Contributions = data[i].Contribution.split("||");
-                        document.getElementById("contributions").innerText = Contributions[z];
-                    }
-                }
-            }
-        }
-    }
-}
-
-function submitForm(id){
-    for(let i = 0;i < data.length;i++){
-        let Module = data[i].Module;
-        for(const [key, value] of Object.entries(data[i])){
-            if(key == "Tasks"){
-                let tasks = value.split("||")
-                for(let z = 0; z < tasks.length;z++){
-                    let dataid = Module + " " + tasks[z];
-                    if(dataid == id){
-                        let Activities = data[i].Activities.split("||");
-                        Activities[z] = document.getElementById("activity").value;
-                        data[i].Activities = Activities.join("||");
-                        let Contribution = data[i].Contribution.split("||");
-                        Contribution[z] = document.getElementById("contribution").value;
-                        data[i].Contribution = Contribution.join("||");;
-                    }
-                }
-            }
-        }
-    } 
-}
-
-function openForm(id){
-    document.getElementById("popupForm").style.display = "block";
-    document.getElementById("infoBox").style.display = "block";
-    infoBoxPop(id);
-    document.getElementById("formTitle").innerText = id;
-}
-
-function closeForm(){
-    document.getElementById("popupForm").style.display = "none";
-    document.getElementById("infoBox").style.display = "none";
-}
-
-function csvToArray(str, delimiter = ",") {
-    const headers = str.slice(0, str.indexOf("\r\n")).split(delimiter);
-    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-    const arr = rows.map(function (row) {
-      const values = row.split(delimiter);
-      const el = headers.reduce(function (object, header, index) {
-        object[header] = values[index];
-        return object;
-      }, {});
-      return el;
-    });
-    return arr;
-}
-
-function populate(){
-    for(let i = 0;i < data.length;i++){
-        let dateStart = data[i].Start.split("||");
-        let dateEnd = data[i].End.split("||");
-        let Percent = data[i].Weighting.split("||");
-        for(const [key, value] of Object.entries(data[i])){
-            if(key == "Tasks"){
-                let tasks = value.split("||")
-                for(let z = 0; z < tasks.length;z++){
-                    var newElement = document.createElement("div");
-                    newElement.innerHTML = data[i].Module;
-                    newElement.classList.add("chart-row-item");
-                    document.getElementById("tasks").appendChild(newElement);
-                    var newList = document.createElement("ul");
-                    newList.classList.add("chart-row-bars");
-                    newList.id = data[i].Module + " " + tasks[z];
-                    newList.setAttribute("onclick","openForm(this.id)");
-                    document.getElementById("tasks").appendChild(newList);
-                    var newItem = document.createElement("li");
-                    newItem.innerHTML = tasks[z] + ":" + Percent[z];
-                    newItem.classList.add("chart-li");
-                    newItem.style.gridColumn = `${dateStart[z]}/${dateEnd[z]}`;
-                    document.getElementById(data[i].Module + " " + tasks[z]).appendChild(newItem);
-                }
-            }
-        }
-    }
-}
-
 //submittion buttons
 const submitRegistration = document.getElementById('SubmitRegistration');
 try{
